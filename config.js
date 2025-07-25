a98f5c1d0b64d9c7a2518647db8c61b090731f44
Suyog <suyog0585@gmail.com>
2025-07-25 14:24:47 +0530

    add new server.js latest1

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CLIENT_ID: "RKUZE6BP5EC4CME7SOHP2FKLZX6HIQR4QJDWCZM257LBLAE4S42Q====",   // Replace with your actual client ID
-    CLIENT_SECRET: "22D55ODDKZDJNUCGSERFD67MNHLDXCC5RVBUF3GJMRPKVNSSGHDQ====", // Replace with your actual client secret
+    CLIENT_ID: "PU2RJE5MYKF3AXFP662MB6V5IQ3TEZDHGU4XIG4IV24EADPYMJWQ====",   // Replace with your actual client ID
+    CLIENT_SECRET: "VWGQWFQ33JLAXQE7PJ22XQCTIYMYX7GOBJAYVM6QJGPUFMI5XMVQ====", // Replace with your actual client secret
     TOKEN_EXPIRY: 3600 // 1 hour token validity
 };
